c45c754b2aa4051b8fd8bb0972307caef76c0ec3
erik-martins <erik.ozzetti.martins@gmail.com>
2024-08-15 01:47:26 -0300

    feat: files static

---
+++ src/main/java/com/client/productionreview/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.client.productionreview.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final FileStorageProperties fileStorageProperties;
+
+    @Autowired
+    public WebMvcConfig(FileStorageProperties fileStorageProperties) {
+        this.fileStorageProperties = fileStorageProperties;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:" + fileStorageProperties.getUploadDir() + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+
+
+}
