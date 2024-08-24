7658f3b0cb42a31b6f64db0c3b6a1a4ca8d54896
erik-martins <erik.ozzetti.martins@gmail.com>
2024-08-24 19:03:19 -0300

    feat:  repository

--- src/main/java/com/client/productionreview/repositories/jpa/ReviewRepository.java
+++ src/main/java/com/client/productionreview/repositories/jpa/ReviewRepository.java
@@ -1,6 +1,6 @@
 package com.client.productionreview.repositories.jpa;
 
-import com.client.productionreview.model.Review;
+import com.client.productionreview.model.jpa.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
