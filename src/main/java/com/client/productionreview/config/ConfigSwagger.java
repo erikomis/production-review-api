684741db6b7e17153747c90b1872925d4d30ccef
erik-martins <erik.ozzetti.martins@gmail.com>
2024-08-24 19:05:56 -0300

    feat:   swagger config and jwt auth

--- src/main/java/com/client/productionreview/config/ConfigSwagger.java
+++ src/main/java/com/client/productionreview/config/ConfigSwagger.java
@@ -1,11 +1,15 @@
 package com.client.productionreview.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class ConfigSwagger {
 
     @Bean
