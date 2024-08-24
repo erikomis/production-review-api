73e680539b0db9d7ce6b30266034829e910e2b7e
erik-martins <erik.ozzetti.martins@gmail.com>
2024-08-24 18:59:47 -0300

    feat: repositories

--- src/main/java/com/client/productionreview/repositories/jpa/PermissionRepository.java
+++ src/main/java/com/client/productionreview/repositories/jpa/PermissionRepository.java
@@ -1,13 +1,12 @@
 package com.client.productionreview.repositories.jpa;
 
-import com.client.productionreview.model.jpa.Category;
+import com.client.productionreview.model.jpa.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    Optional<Category> findByName(String categorieName);
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Optional<Permission> findByName(String name);
 }
--- src/main/java/com/client/productionreview/repositories/jpa/SubCategoryRepository.java
+++ src/main/java/com/client/productionreview/repositories/jpa/SubCategoryRepository.java
@@ -1,5 +1,5 @@
 package com.client.productionreview.repositories.jpa;
-import com.client.productionreview.model.SubCategory;
+import com.client.productionreview.model.jpa.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
