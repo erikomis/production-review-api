d93705d73cf56bea2913099ed59989060714e867
erik-martins <erik.ozzetti.martins@gmail.com>
2024-08-30 18:40:22 -0300

    fix: config cache redis

---
+++ src/main/java/com/client/productionreview/config/CacheConfig.java
@@ -0,0 +1,27 @@
+package com.client.productionreview.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))  // Tempo de vida do cache
+                .disableCachingNullValues();    // Evita armazenar valores nulos
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+}
