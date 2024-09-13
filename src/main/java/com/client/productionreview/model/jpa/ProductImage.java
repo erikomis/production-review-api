ffc5a94c852fea3230fdcc37123a5f1bbbb232f2
erik-martins <erik.ozzetti.martins@gmail.com>
2024-09-13 19:28:42 -0300

    feat :  model ProductImage

---
+++ src/main/java/com/client/productionreview/model/jpa/ProductImage.java
@@ -0,0 +1,46 @@
+package com.client.productionreview.model.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_image")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+
+    @Column(name = "image")
+    private String urlImage;
+
+
+    private String type;
+
+
+    private String filename;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private  LocalDateTime updatedAt;
+
+
+}
